906056d3bfe7c43ea34b0f3d3b3a9e5b10de7556
komagenn <89065599+komagenn@users.noreply.github.com>
2025-04-03 22:56:13 +0900

    天橋立、南丹、向日市を追加

--- src/main/java/京都アプリ.java
+++ src/main/java/京都アプリ.java
@@ -42,10 +42,16 @@ public class 京都アプリ {
         sightseeingSpots.put("東山", Arrays.asList("清水寺", "八坂神社", "高台寺"));
         sightseeingSpots.put("嵐山", Arrays.asList("渡月橋", "竹林の道", "天龍寺"));
         sightseeingSpots.put("伏見", Arrays.asList("伏見稲荷大社", "寺田屋", "月桂冠大倉記念館"));
+        sightseeingSpots.put("天橋立", Arrays.asList("天橋立ビューランド", "元伊勢籠神社", "傘松公園"));
+        sightseeingSpots.put("南丹", Arrays.asList("美山かやぶきの里", "るり渓温泉", "日吉ダム"));
+        sightseeingSpots.put("向日市", Arrays.asList("向日神社", "長岡宮跡", "竹の径"));
 
         gourmetSpots.put("東山", Arrays.asList("湯豆腐の名店", "抹茶スイーツカフェ", "京懐石料理"));
         gourmetSpots.put("嵐山", Arrays.asList("嵐山うどん", "桜餅の老舗", "鯛茶漬け"));
         gourmetSpots.put("伏見", Arrays.asList("酒蔵巡りの居酒屋", "京風おでん", "甘酒専門店"));
+        gourmetSpots.put("天橋立", Arrays.asList("海鮮丼の名店", "黒ちくわ", "天橋立ワイン"));
+        gourmetSpots.put("南丹", Arrays.asList("ジビエ料理", "美山牛乳ソフトクリーム", "地元産そば"));
+        gourmetSpots.put("向日市", Arrays.asList("たけのこ料理", "向日市ラーメン", "手作り和菓子店"));
     }
 
     // 一覧表示
