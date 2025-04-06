bc48f4b17ad029da53969aeca18c7e6738035586
komagenn <89065599+komagenn@users.noreply.github.com>
2025-04-07 08:19:31 +0900

    挟まれたら変わるように

--- src/main/java/オセロアプリ.java
+++ src/main/java/オセロアプリ.java
@@ -7,6 +7,12 @@ public class オセロアプリ {
     private static final char WHITE = 'W';
     private static char[][] board = new char[SIZE][SIZE];
 
+    private static final int[][] DIRECTIONS = {
+        {-1, -1}, {-1, 0}, {-1, 1},
+        {0, -1},          {0, 1},
+        {1, -1},  {1, 0}, {1, 1}
+    };
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         initBoard();
@@ -20,16 +26,14 @@ public class オセロアプリ {
             int row = scanner.nextInt();
             int col = scanner.nextInt();
 
-            // 入力チェック
-            if (row < 0 || row >= SIZE || col < 0 || col >= SIZE || board[row][col] != EMPTY) {
-                System.out.println("無効な入力です。もう一度。");
+            if (!isValidMove(row, col, currentPlayer)) {
+                System.out.println("無効な手です。もう一度。");
                 continue;
             }
 
-            board[row][col] = currentPlayer;
+            placeDisk(row, col, currentPlayer);
             printBoard();
 
-            // プレイヤー交代
             currentPlayer = (currentPlayer == BLACK) ? WHITE : BLACK;
         }
     }
@@ -40,7 +44,6 @@ public class オセロアプリ {
                 board[i][j] = EMPTY;
             }
         }
-        // 初期配置
         board[3][3] = WHITE;
         board[3][4] = BLACK;
         board[4][3] = BLACK;
@@ -61,5 +64,61 @@ public class オセロアプリ {
             System.out.println();
         }
         System.out.println();
+    }
+
+    private static boolean isValidMove(int row, int col, char player) {
+        if (row < 0 || row >= SIZE || col < 0 || col >= SIZE || board[row][col] != EMPTY) {
+            return false;
+        }
+        char opponent = (player == BLACK) ? WHITE : BLACK;
+
+        for (int[] dir : DIRECTIONS) {
+            int r = row + dir[0], c = col + dir[1];
+            boolean foundOpponent = false;
+
+            while (r >= 0 && r < SIZE && c >= 0 && c < SIZE) {
+                if (board[r][c] == opponent) {
+                    foundOpponent = true;
+                } else if (board[r][c] == player && foundOpponent) {
+                    return true;
+                } else {
+                    break;
+                }
+                r += dir[0];
+                c += dir[1];
+            }
+        }
+        return false;
+    }
+
+    private static void placeDisk(int row, int col, char player) {
+        board[row][col] = player;
+        char opponent = (player == BLACK) ? WHITE : BLACK;
+
+        for (int[] dir : DIRECTIONS) {
+            int r = row + dir[0], c = col + dir[1];
+            boolean foundOpponent = false;
+            int steps = 0;
+
+            while (r >= 0 && r < SIZE && c >= 0 && c < SIZE) {
+                if (board[r][c] == opponent) {
+                    foundOpponent = true;
+                    steps++;
+                } else if (board[r][c] == player && foundOpponent) {
+                    // ひっくり返す
+                    int rr = row + dir[0], cc = col + dir[1];
+                    for (int i = 0; i < steps; i++) {
+                        board[rr][cc] = player;
+                        rr += dir[0];
+                        cc += dir[1];
+                    }
+                    break;
+                } else {
+                    break;
+                }
+                r += dir[0];
+                c += dir[1];
+            }
+        }
     }
 }
