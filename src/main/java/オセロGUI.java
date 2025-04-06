3e8104e06c1894b763055ba367f6a10bcaabba34
komagenn <89065599+komagenn@users.noreply.github.com>
2025-04-07 08:51:44 +0900

    修正中

--- src/main/java/オセロGUI.java
+++ src/main/java/オセロGUI.java
@@ -1,18 +1,12 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class オセロGUI extends JFrame {
 
     public static void main(String[] args) {
-        // この一行を追加して、JFrameのインスタンスを作成する前にスレッドが安全に実行されるようにする
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new オセロGUI();
-            }
-        });
+        SwingUtilities.invokeLater(() -> new オセロGUI());
     }
 
     public オセロGUI() {
@@ -20,7 +14,12 @@ public class オセロGUI extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(640, 660);
         setLocationRelativeTo(null); // 画面中央に表示
-        add(new JLabel("オセロGUIの準備中...", SwingConstants.CENTER));
-        setVisible(true); // これを実行しないとウィンドウは表示されない
+        
+        JPanel panel = new JPanel();
+        JButton button = new JButton("スタート");
+        panel.add(button);
+        add(panel);
+        
+        setVisible(true);
     }
 }
