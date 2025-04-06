ca3ccddcf130e837dfb19ccd309a1f270460d7eb
komagenn <89065599+komagenn@users.noreply.github.com>
2025-04-07 08:17:24 +0900

    交互に打てるように

--- src/main/java/オセロアプリ.java
+++ src/main/java/オセロアプリ.java
@@ -1,32 +1,65 @@
+import java.util.Scanner;
+
 public class オセロアプリ {
-    private char[][] board;
+    private static final int SIZE = 8;
+    private static final char EMPTY = '.';
+    private static final char BLACK = 'B';
+    private static final char WHITE = 'W';
+    private static char[][] board = new char[SIZE][SIZE];
 
-    public オセロアプリ() {
-        board = new char[8][8];
-        initializeBoard();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        initBoard();
+        printBoard();
+
+        char currentPlayer = BLACK;
+
+        while (true) {
+            System.out.println("現在のプレイヤー: " + (currentPlayer == BLACK ? "黒(B)" : "白(W)"));
+            System.out.print("行と列を半角スペースで入力（例: 3 4）：");
+            int row = scanner.nextInt();
+            int col = scanner.nextInt();
+
+            // 入力チェック
+            if (row < 0 || row >= SIZE || col < 0 || col >= SIZE || board[row][col] != EMPTY) {
+                System.out.println("無効な入力です。もう一度。");
+                continue;
+            }
+
+            board[row][col] = currentPlayer;
+            printBoard();
+
+            // プレイヤー交代
+            currentPlayer = (currentPlayer == BLACK) ? WHITE : BLACK;
+        }
     }
 
-    private void initializeBoard() {
-        // 全て空白で初期化
-        for (int i = 0; i < 8; i++)
-            for (int j = 0; j < 8; j++)
-                board[i][j] = '-';
-
+    private static void initBoard() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                board[i][j] = EMPTY;
+            }
+        }
         // 初期配置
-        board[3][3] = 'W';
-        board[3][4] = 'B';
-        board[4][3] = 'B';
-        board[4][4] = 'W';
+        board[3][3] = WHITE;
+        board[3][4] = BLACK;
+        board[4][3] = BLACK;
+        board[4][4] = WHITE;
     }
 
-    public void printBoard() {
-        System.out.println("  0 1 2 3 4 5 6 7");
-        for (int i = 0; i < 8; i++) {
+    private static void printBoard() {
+        System.out.print("  ");
+        for (int i = 0; i < SIZE; i++) {
             System.out.print(i + " ");
-            for (int j = 0; j < 8; j++) {
+        }
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < SIZE; j++) {
                 System.out.print(board[i][j] + " ");
             }
             System.out.println();
         }
+        System.out.println();
     }
 }
